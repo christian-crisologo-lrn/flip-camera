cd99285b2eb78c2f5497c857b9a421f3d119480d
christian crisologo <ccrisologo@christian-crisologo.local>
2024-08-24 10:55:05 +1000

    wip

--- src/components/VideoContainer.tsx
+++ src/components/VideoContainer.tsx
@@ -13,10 +13,12 @@ const VideoContainer: React.FC = () => {
     const [streamStatus, setStreamStatus] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [isPlaying, setIsPlaying] = useState<boolean>(true);
+    const [videoPlayingStatus, setVideoPlayingStatus] = useState<string>('');
 
     useEffect(() => {
         const getMediaStream = () => {
-            setStreamStatus('Getting media stream...');
+
+            setStreamStatus('Getting media stream... ');
 
             let constraints: any = {
                 audio: false,
@@ -32,7 +34,7 @@ const VideoContainer: React.FC = () => {
                 constraints.video.facingMode = shouldFaceUser ? 'user' : 'environment';
                 constraints.video.width.min = videoRef.current.clientWidth || 480;
                 constraints.video.height.min = videoRef.current.clientHeight || 480;
-
+                setStreamStatus('Video constraints updated and connecting...');
                 navigator.mediaDevices.getUserMedia(constraints)
                     .then(stream => {
                         videoRef.current!.srcObject = stream;
@@ -52,7 +54,7 @@ const VideoContainer: React.FC = () => {
         if (videoRef.current) {
             getMediaStream();
         }
-    }, [shouldFaceUser, videoRef.current]);
+    }, [shouldFaceUser]);
 
     const startRecording = () => {
         if (videoRef.current && videoRef.current.srcObject) {
@@ -97,66 +99,115 @@ const VideoContainer: React.FC = () => {
         }
     };
 
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.onwaiting = () => {
+                setVideoPlayingStatus('buffering');
+            };
+
+            videoRef.current.onplaying = () => {
+                setVideoPlayingStatus('playing');
+            };
+
+            videoRef.current.onpause = () => {
+                setVideoPlayingStatus('paused');
+            };
+
+            videoRef.current.onended = () => {
+                setVideoPlayingStatus('ended');
+            };
+
+            videoRef.current.onerror = () => {
+                setVideoPlayingStatus('error');
+            };
+
+            videoRef.current.onloadeddata = () => {
+                setVideoPlayingStatus('data loaded');
+            };
+
+            videoRef.current.oncanplay = () => {
+                setVideoPlayingStatus('can play');
+            };
+
+            videoRef.current.oncanplaythrough = () => {
+                setVideoPlayingStatus('can play through');
+            };
+
+            videoRef.current.onstalled = () => {
+                setVideoPlayingStatus('stalled');
+            };
+
+            videoRef.current.onseeked = () => {
+                setVideoPlayingStatus('seeked');
+            };
+
+            videoRef.current.onseeking = () => {
+                setVideoPlayingStatus('seeking');
+            };
+        }
+    }, []);
+
     const recordBtnCls = classname(
         { 'bg-red-500 hover:bg-red-600': isRecording },
         { 'bg-blue-500 hover:bg-blue-600': !isRecording },
-        'mt-2 text-white font-bold py-2 px-4 rounded min-w-[250px]',
+        'mt-2 text-white font-bold py-2 px-4 rounded min-w-[200px]',
     );
 
     return (
-        <div className="container">
-            <div className="p-4 align-center">
-                <div className="mt-2 border align-center p-2">
-                    <div className="flex justify-center items-center relative">
-                        {isLoading && (
-                            <div className="absolute inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-10">
-                                <span className="text-white text-xl">Loading...</span>
-                            </div>
-                        )}
-                        <video
-                            ref={videoRef}
-                            id="camera-stream"
-                            className=""
-                            width={480}
-                            height={480}
-                            autoPlay={true}
-                            playsInline={true}
-                            crossOrigin="anonymous"
-                        ></video>
-                    </div>
+        <div className="p-4 align-center">
+            <div className="mt-2 border align-center p-2">
+                <div className="flex justify-center items-center relative">
+                    {isLoading && (
+                        <div className="absolute inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-10">
+                            <span className="text-white text-xl">Loading...</span>
+                        </div>
+                    )}
+                    <video
+                        ref={videoRef}
+                        id="camera-stream"
+                        className=""
+                        width={480}
+                        height={480}
+                        autoPlay={true}
+                        playsInline={true}
+                        crossOrigin="anonymous"
+                    ></video>
                 </div>
-                <div className="flex mt-4 m-2 flex-col">
-                    <div className="flex my-2 justify-between">
-                        <button
-                            onClick={isPlaying ? pauseVideo : playVideo}
-                            className="mt-2 px-4 py-2 bg-green-500 text-white rounded-md min-w-[150px]"
-                        >
-                            {isPlaying ? 'Pause' : 'Play'}
-                        </button>
-                        <button
-                            onClick={isRecording ? stopRecording : startRecording}
-                            className={recordBtnCls}
-                        >
-                            {isRecording ? 'Stop Recording' : 'Start Recording'}
-                        </button>
+            </div>
+            <div className="flex mt-4 m-2 flex-col">
+                <div className="flex my-2 justify-between">
+                    <button
+                        onClick={isPlaying ? pauseVideo : playVideo}
+                        className="mt-2 px-4 py-2 bg-green-500 text-white rounded-md min-w-[120px]"
+                    >
+                        {isPlaying ? 'Pause' : 'Play'}
+                    </button>
+                    <button
+                        onClick={isRecording ? stopRecording : startRecording}
+                        className={recordBtnCls}
+                    >
+                        {isRecording ? 'Stop Recording' : 'Start Recording'}
+                    </button>
 
-                    </div>
-                    {
-                        showFlipCamera && <button
-                            onClick={toggleCamera}
-                            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
-                        >
-                            Flip Camera {shouldFaceUser ? 'User' : 'Environment'}
-                        </button>
-                    }
+                </div>
+                {
+                    showFlipCamera && <button
+                        onClick={toggleCamera}
+                        className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
+                    >
+                        Flip Camera {shouldFaceUser ? 'User' : 'Environment'}
+                    </button>
+                }
 
-                    <div className="mt-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md">
-                        Stream status : {streamStatus}, videoRef {videoRef?.current?.toString() || 'null'}
-                    </div>
+                <div className="mt-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md">
+                    Stream status : {streamStatus}, videoRef {videoRef?.current?.toString() || 'null'}
+                </div>
 
-                    <div className="mt-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md">
-                        ActiveStream :{activeStream && activeStream.getVideoTracks()[0].label}
-                    </div>
+                <div className="mt-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md">
+                    ActiveStream :{activeStream && activeStream.getVideoTracks()[0].label}
+                </div>
+                <div className="mt-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md">
+                    Stream status :{videoPlayingStatus}
                 </div>
             </div>
         </div>
