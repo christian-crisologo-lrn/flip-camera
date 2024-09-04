5ab870ee4dbbaea46cf827956c86ca952215a09c
christian crisologo <ccrisologo@christian-crisologo.local>
2024-09-04 20:02:10 +1000

    1

--- src/utils/media.ts
+++ src/utils/media.ts
@@ -178,27 +178,27 @@ class MediaDevice {
     toggleVideoFacingMode(callback: Function) {
         console.log('MediaDevices - Toggling video facing mode : ' + this.videoDevices.length);
 
-        if (this.currentDevice && this.videoDevices.length) {
-            const device = this.videoDevices
-                .find((device: any) => {
-                    if (device.deviceId !== this.currentDevice?.deviceId) {
-                        return true;
-                        // if (!device.facingMode.includes(currentFacingMode)) {
-                        //     return true;
-                        // }
-                    }
-                });
+        // if (this.currentDevice && this.videoDevices.length) {
+        //     const device = this.videoDevices
+        //         .find((device: any) => {
+        //             if (device.deviceId !== this.currentDevice?.deviceId) {
+        //                 return true;
+        //                 // if (!device.facingMode.includes(currentFacingMode)) {
+        //                 //     return true;
+        //                 // }
+        //             }
+        //         });
 
-            console.log('MediaDevices - Toggling device: ' + JSON.stringify(device));
+        //     console.log('MediaDevices - Toggling device: ' + JSON.stringify(device));
 
-            if (device) {
-                const facingMode = this.currentDevice.facingMode === 'user' ? 'environment' : 'user';
+        //     if (device) {
+        //         const facingMode = this.currentDevice.facingMode === 'user' ? 'environment' : 'user';
          
-                console.log('MediaDevices - Toggling facing mode: ' + facingMode);
-                
-                // return this.stream(callback, { video: { facingMode: { exact : facingMode } }});
-            }
-        };
+        //         console.log('MediaDevices - Toggling facing mode: ' + facingMode);
+
+        //         return this.stream(callback, { video: { facingMode: { exact : facingMode } }});
+        //     }
+        // };
 
         return Promise.resolve();
     }
