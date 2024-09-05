02a428fc9a9648f86a3b34cdd072b1118fe4f667
christian crisologo <ccrisologo@christian-crisologo.local>
2024-09-05 12:29:26 +1000

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import VideoContainer from './components/VideoContainer'
 import './App.css'
 import React from 'react';
 import Logs from './components/Logs';
+import VideoContainer from './components/VideoContainer';
+import ReactWebCam from './components/ReactWebCam'
+
+const repoURL = "https://api.github.com/repos/christian-crisologo-lrn/flip-camera/commits";
+const fetchLatestCommitHash = async () => {
+  try {
+    const response = await fetch(repoURL);
+    const data = await response.json();
+    return JSON.stringify((data[0].sha));
+  } catch (error) {
+    console.error('Error fetching commit hash:', error);
+  }
+
+  return '';
+};
+
 
 function App() {
 
+  const [hash, setHash] = React.useState<string>('');
+  const [showVideoContainer, setShowVideoContainer] = React.useState<boolean>(true);
+
   React.useEffect(() => {
+
+    const fetchAndSetHash = async () => {
+      const newHash = await fetchLatestCommitHash();
+      setHash(newHash);
+    };
+
     const logContainer = document.getElementById('log-container');
 
     window.console.log = console.log =
@@ -17,17 +41,31 @@ function App() {
         logContainer?.appendChild(logMessage);
       };
 
+    fetchAndSetHash();
+
     return () => {
     };
   }, []);
 
+  const toggleComponent = () => {
+    setShowVideoContainer((prevState: any) => !prevState);
+  };
+
   return (
     <div className="bg-gray-100 dark:bg-gray-700 flex flex-col items-center justify-center w-full">
-      <div className='container max-w-[420px]'>
+      <div className='container max-w-[420px] items-center justify-center text-center '>
         <h1 className="text-2xl mt-2 text-center font-bold text-black dark:text-white">Flip Camera</h1>
-        <VideoContainer />
+        <button
+          onClick={toggleComponent}
+          className="bg-gray-500 text-white text-sm px-4 py-2 rounded mt-4 self-center"
+        >
+          Toggle component: {showVideoContainer ? 'ReactWebCam' : 'VideoContainer'}
+        </button>
+
+        {showVideoContainer ? <VideoContainer /> : <ReactWebCam />}
         <Logs />
       </div>
+      <p className='min-w-[400px] mb-2 text-center text-gray-500 bg-black border border-gray-400 rounded text-xs p-1 '>version : {hash}</p>
     </div>
   )
 }
---
+++ src/components/ReactWebCam.tsx
@@ -0,0 +1,115 @@
+import React, { useRef, useState } from 'react';
+import classname from 'classnames';
+import RecordRTC, { invokeSaveAsDialog } from 'recordrtc';
+import WebCam from '../utils/react-webcam';
+
+const VideoContainer: React.FC = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [recorder, setRecorder] = useState<RecordRTC | null>(null);
+    const [isRecording, setIsRecording] = useState<boolean>(false);
+    // const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isPlaying, setIsPlaying] = useState<boolean>(true);
+    const [facingMode, setFacingMode] = useState<string>('user');
+    // const [videoPlayingStatus, setVideoPlayingStatus] = useState<string>('idle');
+    // const [cameraDevices, setCameraDevices] = useState<any>([]);
+    const [messages, setMessages] = useState<any>([]);
+
+    const startRecording = () => {
+        if (videoRef.current && videoRef.current.srcObject) {
+            const stream = videoRef.current.srcObject as MediaStream;
+            const newRecorder = new RecordRTC(stream, { type: 'video' });
+            newRecorder.startRecording();
+            setRecorder(newRecorder);
+            setIsRecording(true);
+        }
+    };
+
+    const stopRecording = () => {
+        if (recorder) {
+            recorder.stopRecording(() => {
+                invokeSaveAsDialog(recorder.getBlob());
+                setIsRecording(false);
+            });
+        }
+    };
+
+    const playVideo = () => {
+        if (videoRef.current) {
+            videoRef.current.play();
+            setIsPlaying(true);
+        }
+    };
+
+    const pauseVideo = () => {
+        if (videoRef.current) {
+            videoRef.current.pause();
+            setIsPlaying(false);
+        }
+    };
+
+    const toggleCamera = () => {
+        setMessages([...messages, `Toggling camera facing mode: ${facingMode}`]);
+        setFacingMode(facingMode === 'user' ? 'environment' : 'user');
+    };
+
+    // const onVideoRefChange = (ref: HTMLVideoElement) => {
+    //     if (ref) {
+    //         videoRef = ref;
+    //     }
+    // }
+
+    const recordBtnCls = classname(
+        { 'bg-red-500 hover:bg-red-600': isRecording },
+        { 'bg-blue-500 hover:bg-blue-600': !isRecording },
+        'mt-4 text-white font-bold py-2 px-4 rounded',
+    );
+
+    return (
+        <div className="m-1">
+            <div className="container p-5 align-center">
+                <div className="mt-2 border align-center p-5">
+                    <div className="flex justify-center items-center relative">
+                        {/* {isLoading && (
+                            <div className="absolute inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-10">
+                                <span className="text-white text-xl">Loading...</span>
+                            </div>
+                        )} */}
+                        <WebCam
+                            audio={false}
+                            videoConstraints={{ facingMode: 'user', width: 300, height: 285 }} />
+                    </div>
+                </div>
+                <div className="flex mt-4 m-2 flex-col">
+                    <div className="flex my-2 justify-between">
+                        <button
+                            onClick={isPlaying ? pauseVideo : playVideo}
+                            className="mt-4 font-bold py-2 px-4 bg-green-500 text-white rounded-md min-w-[200px]"
+                        >
+                            {isPlaying ? 'Pause' : 'Play'}
+                        </button>
+                        <button
+                            onClick={isRecording ? stopRecording : startRecording}
+                            className={recordBtnCls}
+                        >
+                            {isRecording ? 'Stop Recording' : 'Start Recording'}
+                        </button>
+                    </div>
+                    <button
+                        onClick={toggleCamera}
+                        className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
+                    >
+                        Flip Camera
+                    </button>
+                    {/* <div className="mt-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md">
+                        Video status : [{videoPlayingStatus}]
+                    </div> */}
+                    <div className="mt-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md">
+                        Messages : {messages.map((message: any, index: number) => (<p key={index}>{message}</p>))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoContainer;
